361f19edbc03b096a693c4a6acabfadb7df3b258
YanGraim <yangraim@gmail.com>
2025-05-07 16:54:43 -0300

    estiliza component header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
     <main>
-      <h1>TEste</h1>
+      <div style={{ width: "100%", height: 600, backgroundColor: "red" }}>
+        <h1>teste</h1>
+      </div>
+      <div style={{ width: "100%", height: 600, backgroundColor: "blue" }}>
+        <h1>teste</h1>
+      </div>
+      <div style={{ width: "100%", height: 600, backgroundColor: "green" }}>
+        <h1>teste</h1>
+      </div>
     </main>
   );
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,36 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import styles from "./styles.module.scss";
+
 export function Header() {
+  const [top, setTop] = useState(true);
+
+  const scrollHandler = () => {
+    window.scrollY > 8 ? setTop(false) : setTop(true);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", scrollHandler);
+
+    return () => removeEventListener("scroll", scrollHandler);
+  }, [top]);
   return (
-    <div>
-      <h1>Teste</h1>
-    </div>
+    <header
+      className={`${styles.header} ${!top ? styles.fixed : styles.background}`}
+    >
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.logo}>
+            <Link href={"/"}>Dev Motors</Link>
+          </div>
+          <nav className={styles.nav}>
+            <Link href={"/"}>HOME</Link>
+            <Link href={"/#servicos"}>SERVIÇOS</Link>
+            <Link href={"/#contatos"}>CONTATOS</Link>
+          </nav>
+        </div>
+      </div>
+    </header>
   );
 }

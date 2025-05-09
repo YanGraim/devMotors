d2e02f160e8dcc57e1a634443e1aa01eb5394ef4
YanGraim <yangraim@gmail.com>
2025-05-09 09:05:59 -0300

    ajusta responsividade do component header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <div style={{ width: "100%", height: 600, backgroundColor: "red" }}>
-        <h1>teste</h1>
-      </div>
-      <div style={{ width: "100%", height: 600, backgroundColor: "blue" }}>
-        <h1>teste</h1>
-      </div>
-      <div style={{ width: "100%", height: 600, backgroundColor: "green" }}>
-        <h1>teste</h1>
-      </div>
+      <h1>teste</h1>
     </main>
   );
 }

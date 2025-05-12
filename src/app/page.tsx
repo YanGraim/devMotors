9ecb25fb8126cf0c95298adc62f50d2453f710be
YanGraim <yangraim@gmail.com>
2025-05-12 11:20:12 -0300

    feat: cria e estiliza component Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
+import { Hero } from "@/components/hero";
 import { Submenu } from "@/components/home/submenu";
 import { getDataHome } from "@/utils/actions/get-data";
+import { HomeProps } from "@/utils/home.type";
+import { Phone } from "lucide-react";
 
 export default async function Home() {
-  const data = await getDataHome();
-  console.log(data);
+  const { object }: HomeProps = await getDataHome();
+  console.log(object.title);
 
   return (
     <main>
       <Submenu />
+      <Hero
+        heading={object.metadata.heading}
+        buttonTitle={object.metadata.cta_button.title}
+        buttonUrl={object.metadata.cta_button.url}
+        bannerUrl={object.metadata.banner.url}
+        icon={<Phone size={24} color="#fff" />}
+      />
     </main>
   );
 }

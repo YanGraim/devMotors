8dfe850cade5bedc0109368c3bf178860c39d952
YanGraim <yangraim@gmail.com>
2025-05-12 16:28:08 -0300

    estiliza area about

--- src/components/home/services/index.tsx
+++ src/components/home/services/index.tsx
@@ -4,20 +4,40 @@ import Image from "next/image";
 
 export function Services({ object }: HomeProps) {
   return (
-    <section className={styles.containerAbout}>
-      <article className={styles.innerAbout}>
-        <h1 className={styles.title}>Sobre</h1>
-        <p>{object.metadata.about.description}</p>
-      </article>
-      <div className={styles.bannerAbout}>
-        <Image
-          src={object.metadata.about.banner.url}
-          alt="Imagem sobre a empresa"
-          quality={100}
-          fill={true}
-          className={styles.imageAbout}
-        />
-      </div>
-    </section>
+    <>
+      <section className={styles.containerAbout}>
+        <article className={styles.innerAbout}>
+          <h1 className={styles.title}>Sobre</h1>
+          <p>{object.metadata.about.description}</p>
+        </article>
+        <div className={styles.bannerAbout}>
+          <Image
+            src={object.metadata.about.banner.url}
+            alt="Imagem sobre a empresa"
+            quality={100}
+            fill={true}
+            className={styles.imageAbout}
+          />
+        </div>
+      </section>
+
+      <h2 className={styles.servicesTitle}>Conheça nossos serviços</h2>
+      <section className={styles.services}>
+        {object.metadata.services.map((service) => (
+          <article key={service.description} className={styles.service}>
+            <div className={styles.innerService}>
+              <Image
+                src={service.image.url}
+                alt="Imagem sobre o serviço"
+                quality={100}
+                fill={true}
+                className={styles.imageService}
+              />
+            </div>
+            <p>{service.description}</p>
+          </article>
+        ))}
+      </section>
+    </>
   );
 }

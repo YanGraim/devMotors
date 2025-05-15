631ce7585bd5a4f02081615ddc91d93b11317bd0
YanGraim <yangraim@gmail.com>
2025-05-15 17:07:21 -0300

    aplica suspense ui

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,11 +1,10 @@
+import { Suspense } from "react";
+
 import { getItemBySlug } from "@/utils/actions/get-data";
-import styles from "./styles.module.scss";
 import { PostProps } from "@/utils/post.type";
-import { Hero } from "@/components/hero";
-import { Phone } from "lucide-react";
-import { Container } from "@/components/container";
-import Image from "next/image";
 import { Metadata } from "next";
+import { Content } from "./components/content";
+import { LoadingPost } from "./components/loading";
 
 export async function generateMetadata({
   params: { slug },
@@ -51,48 +50,11 @@ export default async function Page({
 }: {
   params: { slug: string };
 }) {
-  const { objects }: PostProps = await getItemBySlug(slug);
-
   return (
     <>
-      <Hero
-        heading={objects[0].title}
-        buttonTitle={objects[0].metadata.button.title}
-        buttonUrl={objects[0].metadata.button.url}
-        bannerUrl={objects[0].metadata.banner.url}
-        icon={<Phone size={24} color="#fff" />}
-      />
-      <Container>
-        <section className={styles.about}>
-          <article className={styles.innerAbout}>
-            <h1 className={styles.title}>
-              {objects[0].metadata.description.title}
-            </h1>
-            <p>{objects[0].metadata.description.text}</p>
-
-            {objects[0].metadata.description.button_active && (
-              <a
-                href={objects[0].metadata.description.button_url as string}
-                target="_blank"
-                className={styles.link}
-              >
-                {objects[0].metadata.description.button_title}
-              </a>
-            )}
-          </article>
-          <div className={styles.bannerAbout}>
-            <Image
-              src={objects[0].metadata.banner.url}
-              alt={objects[0].title}
-              fill={true}
-              quality={100}
-              priority={true}
-              sizes="(max-width: 480px) 100vw, (max-width: 1024px) 75vw, 50vw"
-              className={styles.imageAbout}
-            />
-          </div>
-        </section>
-      </Container>
+      <Suspense fallback={<LoadingPost />}>
+        <Content slug={slug} />
+      </Suspense>
     </>
   );
 }

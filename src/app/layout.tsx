3f7141deccc4e0f44a7203306858bb23ffe0ad93
YanGraim <yangraim@gmail.com>
2025-05-15 15:17:30 -0300

    cria metadata da pagina home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,21 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "DevMotors - Sua oficina especializada!",
   description: "Oficina de automoveis.",
+  keywords: ["oficina", "oficina de carros", "manutenção de carros"],
+  openGraph: {
+    title: "DevMotors - Sua oficina especializada!",
+    // images: ["aqui vai uma imagem ou uma url de imagem"]
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+    },
+  },
 };
 
 export default function RootLayout({

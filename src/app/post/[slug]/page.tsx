f30cd7801ae4a0b171636662aa97becfa3dd1d9a
YanGraim <yangraim@gmail.com>
2025-05-15 16:23:49 -0300

    adiciona metadata de maneira dinamica

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -5,6 +5,46 @@ import { Hero } from "@/components/hero";
 import { Phone } from "lucide-react";
 import { Container } from "@/components/container";
 import Image from "next/image";
+import { Metadata } from "next";
+
+export async function generateMetadata({
+  params: { slug },
+}: {
+  params: { slug: string };
+}): Promise<Metadata> {
+  try {
+    const { objects }: PostProps = await getItemBySlug(slug).catch(() => {
+      return {
+        title: "DevMotors - Sua oficina especializada!",
+        description: "Oficina de automoveis.",
+      };
+    });
+
+    return {
+      title: `DevMotors - ${objects[0].title}`,
+      description: `${objects[0].metadata.description}`,
+      openGraph: {
+        title: `DevMotors - ${objects[0].title}`,
+        images: [objects[0].metadata.banner.url],
+      },
+      robots: {
+        index: true,
+        follow: true,
+        nocache: true,
+        googleBot: {
+          index: true,
+          follow: true,
+          noimageindex: true,
+        },
+      },
+    };
+  } catch (error) {
+    return {
+      title: "DevMotors - Sua oficina especializada!",
+      description: "Oficina de automoveis.",
+    };
+  }
+}
 
 export default async function Page({
   params: { slug },

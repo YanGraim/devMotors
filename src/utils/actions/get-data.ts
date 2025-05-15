3ed1ebd57694fcd87b149191508f51df92737bc8
YanGraim <yangraim@gmail.com>
2025-05-15 15:05:44 -0300

    adiciona redirect caso ocorra erro de chamada de rota

--- src/utils/actions/get-data.ts
+++ src/utils/actions/get-data.ts
@@ -1,3 +1,5 @@
+import { redirect } from "next/navigation";
+
 export async function getDataHome() {
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/objects/681e08f908956c95e3c71895?pretty=true&read_key=${process.env.READ_KEY}&depth=1&props=slug,title,metadata,type`, { next: { revalidate: 120 } });
@@ -43,6 +45,6 @@ export async function getItemBySlug(itemSlug: string) {
     };
     return res.json();
   } catch (error) {
-    throw new Error("Faile get item by slug");
+    redirect("/");
   }
 }
